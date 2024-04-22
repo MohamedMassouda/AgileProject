dc3a35e32ff869816251d1382c08c5b42cbfdfda
Ahmed-Messaoudi <ahmed.massoudi@horizon-tech.tn>
2024-04-22 12:09:34 +0100

    nav bar and footer edition

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./screens/Navbar.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./screens/SignUp.jsx";
+import Footer from "./screens/Footer.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navbar />} />
         <Route path="/register" element={<SignUp />} />
+        <Route path="/footer" element={<Footer />} />
       </Routes>
     </BrowserRouter>
   );
