f903ff5b3eeb389af2f94061fde7ef60bee88a3c
slamasamar4 <110769131+slamasamar4@users.noreply.github.com>
2024-05-01 09:26:45 +0100

    profil

---
+++ frontend/src/components/profile/App.js
@@ -0,0 +1,17 @@
+// App.js
+
+import React from 'react';
+import './App.css';
+import FullSideBar from './FullSideBar';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='AppGlass'>
+        <FullSideBar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
